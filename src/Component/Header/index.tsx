4750ce5212ed210481a7fa66336495f3b3c9c779
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-05-05 20:05:13 -0300

    criando a notificação da quantidade de item no carrinho

--- src/Component/Header/index.tsx
+++ src/Component/Header/index.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
 export function Header() {
   return (
     <div>
-      <h2>Eu sou o Header</h2>
+      <header className="bg-gray-900 w-full p-2">
+        <nav className="w-full max-w-7xl flex  items-center justify-between mx-auto h-14 ">
+          <Link
+            to="/"
+            className="font-bold text-3xl  bg-gradient-to-r from-green-600 via-yellow-500 to-blue-700 bg-clip-text text-transparent"
+          >
+            BRASIL_SHOP
+          </Link>
+          <Link className="mt-6 relative" to={"/carts"}>
+            <FiShoppingCart size={24} color="white" />
+            <span className="bg-lime-600 flex items-center justify-center font-bold rounded-full text-white absolute -top-4 -right-3 text-xs w-5 h-5">
+              2
+            </span>
+          </Link>
+        </nav>
+      </header>
     </div>
   );
 }
