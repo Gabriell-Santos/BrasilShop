8e92bdf0701dd4b913a29c82becdbf0b507f35c3
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-05-12 19:40:08 -0300

    mostrando os dados do produto na tela

--- src/Pages/Details/index.tsx
+++ src/Pages/Details/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { FiTrash2 } from "react-icons/fi";
-import logo from "../../assets/teste.jpeg";
+
 import { useContext, useState } from "react";
 import { CartContext } from "../../Services/ContextApi/index";
 import { useParams } from "react-router-dom";
@@ -16,7 +16,7 @@ interface ProdutoProps {
 }
 
 export function Details() {
-  const [product, setproduct] = useState();
+  const [product, setproduct] = useState<ProdutoProps>();
   const { addItemCart } = useContext(CartContext);
   const { id } = useParams();
 
@@ -38,34 +38,39 @@ export function Details() {
         Detalhes do Produto
       </h2>
 
-      <section className="flex items-start gap-6 w-full max-w-2xl mt-7 ">
-        {/* Imagem */}
-        <div className="w-48 h-48 flex-shrink-0">
-          <img
-            className="w-full h-full object-cover rounded-lg"
-            src={logo}
-            alt="Produto"
-          />
-        </div>
-
-        {/* Texto */}
-        <div className="flex flex-col flex-1">
-          <h3 className="font-medium text-lg">Tênis Nike Air Max 270</h3>
-          <p className="text-gray-500 text-sm mt-2">
-            O Nike Air Max 270 é um tênis de corrida e estilo de vida que
-            combina conforto e design.
-          </p>
-
-          <div className="flex items-center justify-between mt-4">
-            <span className="font-bold text-xl">R$ 599,99</span>
-            <Link to={"/carts"}>
-              <button>
-                <FiTrash2 size={20} />
-              </button>
-            </Link>
+      {product && (
+        <section className="flex items-start gap-6 w-full max-w-2xl mt-7 ">
+          {/* Imagem */}
+          <div className="w-48 h-48 flex-shrink-0">
+            <img
+              className="w-full h-full object-cover rounded-lg"
+              src={product.cover}
+              alt={product.title}
+            />
           </div>
-        </div>
-      </section>
+
+          {/* Texto */}
+          <div className="flex flex-col flex-1">
+            <h3 className="font-medium text-lg"> {product.title} </h3>
+            <p className="text-gray-500 text-sm mt-2">{product.description}</p>
+
+            <div className="flex items-center justify-between mt-4">
+              <span className="font-bold text-xl">
+                R$
+                {product.price.toLocaleString("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+              <Link to={"/carts"}>
+                <button onClick={() => addItemCart(product)}>
+                  <FiTrash2 size={20} />
+                </button>
+              </Link>
+            </div>
+          </div>
+        </section>
+      )}
     </div>
   );
 }
